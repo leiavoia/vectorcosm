f3fc3443733779ba33acf6cbd804e6e4221522af
leiavoia <leiavoia@users.noreply.github.com>
2023-09-03 10:26:03 -0700

    UI QoL

--- src/App.vue
+++ src/App.vue
@@ -25,6 +25,7 @@ window.vc = vc;
 window.vc.onSimulationChange = new_sim => { sim.value = new_sim; }
 
 let dragging = false;
+let show_boid_details = ref(false);
 
 let boidviewer = new Two({ fitted: true, type: 'SVGRenderer' }); 
 let braingraph_context = new Two({ fitted: true, type: 'SVGRenderer' }); 
@@ -32,55 +33,55 @@ let braingraph = null;
 
 let focus_boid_data = ref();
 
-// // subscriptions to critical events
-// let frameUpdateSubscription = PubSub.subscribe('frame-update', (msg,data) => {
-// 	// copy data from boid in focus. 
-// 	// it would nice to just reactify the boid itself, 
-// 	// but its considered high performance data and we need to stay hands off
-// 	if ( vc.focus_object ) {
-// 		if ( !focus_boid_data.value ) { focus_boid_data.value = {}; }
-// 		for ( let i of ['id','species','generation','max_energy','energy','diet','diet_range',
-// 			'length','width','inertia','angmo','energy_cost','total_fitness_score','stomach_contents','stomach_size',
-// 			'age', 'lifespan', 'maturity_age' ] ) {
-// 			focus_boid_data.value[i] = vc.focus_object[i];
-// 		}
-// 		focus_boid_data.value.sensors = vc.focus_object.sensors.map(s => ({name:s.name||s.detect, val:s.val}) );
-// 		focus_boid_data.value.outputs = vc.focus_object.brain.nodes
-// 			.filter(n => n.type=='output')
-// 			.map(n => ({val:n.activation.toFixed(2)}) );
-// 		focus_boid_data.value.outputs.forEach( (n,i) => n.name = vc.focus_object.motors[i].name );
-// 		focus_boid_data.value.motors = vc.focus_object.motors.map( m => ({
-// 			name: m.name,
-// 			strokefunc: (m.strokefunc || 'constant'),
-// 			t: m.t,
-// 			min_act: m.min_act,
-// 			linear: m.linear,
-// 			angular: m.angular,
-// 			wheel: m.wheel,
-// 			stroketime: m.stroketime,
-// 			this_stoke_time: (m.this_stoke_time||0),
-// 			strokepow: (m.strokepow||0),
-// 			cost: m.cost,
-// 			last_amount: (m.last_amount||0)
-// 		}) );
-// 		focus_boid_data.value.brainnodes = vc.focus_object.brain.nodes.map( n => {
-// 			let hexval = utils.DecToHex( Math.round(Math.abs(utils.clamp(n.activation,-1,1)) * 255) );
-// 			return {
-// 				symbol: (n.type=='input' ? 'I' : ( n.type=='output' ? 'O' : n.squash.name.charAt(0) ) ),
-// 				color: ( n.activation >= 0 ? ('#00' + hexval + '00') : ('#' + hexval + '0000') ),
-// 				type: n.type
-// 			};
-// 		});
-// 		if ( braingraph ) { 
-// 			braingraph.setTarget(vc.focus_object);
-// 			braingraph.Draw();
-// 			braingraph_context.update();		
-// 		}
-// 	}
-// 	else if ( focus_boid_data.value ) {
-// 		focus_boid_data.value = null;
-// 	}
-// });
+// subscriptions to critical events
+let frameUpdateSubscription = PubSub.subscribe('frame-update', (msg,data) => {
+	// copy data from boid in focus. 
+	// it would nice to just reactify the boid itself, 
+	// but its considered high performance data and we need to stay hands off
+	if ( vc.focus_object ) {
+		if ( !focus_boid_data.value ) { focus_boid_data.value = {}; }
+		for ( let i of ['id','species','generation','max_energy','energy','diet','diet_range',
+			'length','width','inertia','angmo','energy_cost','total_fitness_score','stomach_contents','stomach_size',
+			'age', 'lifespan', 'maturity_age' ] ) {
+			focus_boid_data.value[i] = vc.focus_object[i];
+		}
+		focus_boid_data.value.sensors = vc.focus_object.sensors.map(s => ({name:s.name||s.detect, val:s.val}) );
+		focus_boid_data.value.outputs = vc.focus_object.brain.nodes
+			.filter(n => n.type=='output')
+			.map(n => ({val:n.activation.toFixed(2)}) );
+		focus_boid_data.value.outputs.forEach( (n,i) => n.name = vc.focus_object.motors[i].name );
+		focus_boid_data.value.motors = vc.focus_object.motors.map( m => ({
+			name: m.name,
+			strokefunc: (m.strokefunc || 'constant'),
+			t: m.t,
+			min_act: m.min_act,
+			linear: m.linear,
+			angular: m.angular,
+			wheel: m.wheel,
+			stroketime: m.stroketime,
+			this_stoke_time: (m.this_stoke_time||0),
+			strokepow: (m.strokepow||0),
+			cost: m.cost,
+			last_amount: (m.last_amount||0)
+		}) );
+		focus_boid_data.value.brainnodes = vc.focus_object.brain.nodes.map( n => {
+			let hexval = utils.DecToHex( Math.round(Math.abs(utils.clamp(n.activation,-1,1)) * 255) );
+			return {
+				symbol: (n.type=='input' ? 'I' : ( n.type=='output' ? 'O' : n.squash.name.charAt(0) ) ),
+				color: ( n.activation >= 0 ? ('#00' + hexval + '00') : ('#' + hexval + '0000') ),
+				type: n.type
+			};
+		});
+		if ( braingraph ) { 
+			braingraph.setTarget(vc.focus_object);
+			braingraph.Draw();
+			braingraph_context.update();		
+		}
+	}
+	else if ( focus_boid_data.value ) {
+		focus_boid_data.value = null;
+	}
+});
 
 
 // input events
@@ -153,9 +154,15 @@ const keyFunctionMap = {
 		},
 	'PageUp': _ => {
 			vc.ShiftFocusTarget();
+			if ( show_boid_details.value ) {
+				RefreshBoidDetailsDynamicObjects( vc.focus_object );
+			}			
 		},
 	'PageDown': _ => {
 			vc.ShiftFocusTarget(-1);
+			if ( show_boid_details.value ) {
+				RefreshBoidDetailsDynamicObjects( vc.focus_object );
+			}			
 		},
 	'ScrollLock': _ => {
 			vc.ResizeTankToWindow();
@@ -187,11 +194,26 @@ const keyFunctionMap = {
 	'End': _ => {
 			vc.ToggleSimulatorFF();
 		},
+	'Escape': _ => {
+			if ( show_boid_details.value ) { show_boid_details.value = false; }
+			else if ( vc.focus_object ) { vc.StopTrackObject(); }
+		},
+	'i': _ => {
+			if ( vc.focus_object ) {
+				show_boid_details.value = !show_boid_details.value;
+				if ( show_boid_details.value ) {
+					RefreshBoidDetailsDynamicObjects( vc.focus_object );
+				}
+			}
+		},
 	't': _ => {
 			if ( vc.focus_object ) { vc.StopTrackObject(); }
 			else {
 				const b = vc.tank.boids.sort( (a,b) => b.total_fitness_score - a.total_fitness_score )[0];
 				vc.TrackObject(b);
+				if ( show_boid_details.value ) {
+					RefreshBoidDetailsDynamicObjects( vc.focus_object );
+				}						
 			}
 		},
 	'l': _ => {
@@ -265,33 +287,9 @@ function ClickMap( event ) {
 		}
 	}
 	if ( closest ) {
+		show_boid_details.value = true;
 		vc.TrackObject(closest);
-		// let timeout = setTimeout( _ => {
-		// 	boidviewer.clear();
-		// 	let elem = document.getElementById('boidviewer');
-		// 	boidviewer.appendTo(elem);
-		// 	let geo = closest.body.geo.clone();
-		// 	geo.dashes = closest.body.geo.dashes;
-		// 	console.log(geo);
-		// 	geo.position.x = boidviewer.width * 0.5;
-		// 	geo.position.y = boidviewer.height * 0.5;
-		// 	geo.rotation = -Math.PI * 0.5;
-		// 	const bounds = geo.getBoundingClientRect();
-		// 	geo.scale = Math.max(boidviewer.width, boidviewer.height) / Math.max( bounds.height, bounds.width );
-		// 	geo.scale *= 0.90; // whitespace
-		// 	// geo.center();
-		// 	boidviewer.add(geo);
-		// 	boidviewer.update();
-			
-		// 	// braingraphing
-		// 	braingraph_context.clear();
-		// 	let braingraph_elem = document.getElementById('braingraph');
-		// 	braingraph_context.appendTo(braingraph_elem);
-		// 	braingraph = new BrainGraph(null,braingraph_context);
-		// 	braingraph.setTarget(closest);
-		// 	braingraph.Draw();
-		// 	braingraph_context.update();
-		// }, 100 );
+		RefreshBoidDetailsDynamicObjects(closest);
 	}
 	else {
 		vc.StopTrackObject();
@@ -300,6 +298,35 @@ function ClickMap( event ) {
 			braingraph = null;
 		}
 	}
+}
+
+function RefreshBoidDetailsDynamicObjects(obj) {
+	setTimeout( _ => {
+		boidviewer.clear();
+		let elem = document.getElementById('boidviewer');
+		boidviewer.appendTo(elem);
+		let geo = obj.body.geo.clone();
+		geo.dashes = obj.body.geo.dashes;
+		console.log(geo);
+		geo.position.x = boidviewer.width * 0.5;
+		geo.position.y = boidviewer.height * 0.5;
+		geo.rotation = -Math.PI * 0.5;
+		const bounds = geo.getBoundingClientRect();
+		geo.scale = Math.max(boidviewer.width, boidviewer.height) / Math.max( bounds.height, bounds.width );
+		geo.scale *= 0.90; // whitespace
+		// geo.center();
+		boidviewer.add(geo);
+		boidviewer.update();
+			
+		// braingraphing
+		braingraph_context.clear();
+		let braingraph_elem = document.getElementById('braingraph');
+		braingraph_context.appendTo(braingraph_elem);
+		braingraph = new BrainGraph(null,braingraph_context);
+		braingraph.setTarget(obj);
+		braingraph.Draw();
+		braingraph_context.update();
+	}, 100 );
 }
 
 </script>
@@ -318,18 +345,18 @@ function ClickMap( event ) {
 		@mousedown="MouseDown($event)"
 		@mouseup="MouseUp($event)"
 		>
-		<section class="iconmenu" v-if="focus_boid_data" style="display:flex; flex-flow: column;">
+		<section class="iconmenu" v-if="show_boid_details && focus_boid_data" style="display:flex; flex-flow: column;">
 			<button style="width:100%; padding:0.5em; margin: 0 0 0.25em; display:block;">Exit</button>
 			<button style="width:100%; padding:0.5em; margin: 0 0 0.25em; display:block;">Save Specimen</button>
 		</section>
-		<section class="boid-braingraph-panel" v-if="focus_boid_data">
+		<section class="boid-braingraph-panel" v-if="show_boid_details && focus_boid_data">
 			<div id="braingraph"  style="width:100%; height: 100%;"></div>
 		</section>
-		<section class="boid-detail" v-if="focus_boid_data">
+		<section class="boid-detail" v-if="show_boid_details && focus_boid_data">
 			<h2 style="text-align:center;">{{focus_boid_data.species.toUpperCase()}}</h2>
 			<br/>
 			<!-- <div id="boidviewer"  style="width:12em; height: 12em; background:transparent; border-radius:0.5em; float:right; margin: 0 0 1em 1em; box-sizing: border-box;"></div> -->
-			<div id="boidviewer"  style="width:100%; aspect-ratio:1;"></div>
+			<div id="boidviewer"  style="width:10em; aspect-ratio:1; margin: 0 auto"></div>
 			<br/>
 			<p>ID: {{focus_boid_data.id}}</p>
 			<p>GENERATION: {{focus_boid_data.generation}}</p>
@@ -365,7 +392,7 @@ function ClickMap( event ) {
 			</p> -->
 			
 		</section>
-		<section class="boid-motors" v-if="focus_boid_data">			
+		<section class="boid-motors" v-if="show_boid_details && focus_boid_data">			
 			<h2>Motors</h2>
 			<div v-for="m of focus_boid_data.motors">
 				<!-- <progress :value="m.this_stoke_time ? (m.strokepow * (1-(m.t / m.this_stoke_time))) : 0"></progress> -->
@@ -373,18 +400,19 @@ function ClickMap( event ) {
 				<!-- <progress :value="m.this_stoke_time ? m.strokepow : 0"></progress>				 -->
 				<progress :value="m.this_stoke_time ? m.last_amount : 0"></progress>
 				&nbsp;
-				{{m.name}}, <i>{{m.strokefunc}}</i>, &gt;{{m.min_act.toFixed(2)}}
+				{{m.name}}, <i>{{m.strokefunc}}</i>, &gt;{{m.min_act.toFixed(2)}}, t{{m.stroketime.toFixed(2)}}, ${{m.cost.toFixed(2)}}
+				
 			</div>
 			
 		</section>
-		<section class="boid-sensors" v-if="focus_boid_data">			
+		<section class="boid-sensors" v-if="show_boid_details && focus_boid_data">			
 			<h2>Sensors</h2>
 			<div v-for="i of focus_boid_data.sensors">
 				<progress :value="i.val"></progress> &nbsp; {{i.name}}
 			</div>
 			
 		</section>
-		<section class="boid-brain-output" v-if="focus_boid_data">			
+		<section class="boid-brain-output" v-if="show_boid_details && focus_boid_data">			
 			<h2>NN Outputs</h2>
 			<div v-for="o of focus_boid_data.outputs">
 				<progress :value="o.val"></progress> &nbsp; {{o.name}}
@@ -405,7 +433,7 @@ function ClickMap( event ) {
 		visibility: visible;		
 		/* border: 1px solid white; */
 		display:grid;
-		grid-template-columns: 10rem 0.65fr 0.65fr 30em;
+		grid-template-columns: 10rem 0.65fr 0.65fr 28em;
 		grid-template-rows: 1fr 1fr;
 		gap: 1rem;
 	}
