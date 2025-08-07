bd22085067065d84065f53cede2f18d284406137
leiavoia <leiavoia@users.noreply.github.com>
2025-08-06 17:01:28 -0700

    Fixed edibility bug on low food counts.

--- src/classes/class.Boids.js
+++ src/classes/class.Boids.js
@@ -499,10 +499,8 @@ export class Boid {
 				const r = this.collision.radius + grace;
 				// get a list of collision candidates
 				let foods = this.tank.foods; // runs faster on small sets
-				if ( foods.length > 20 ) {
-					const test = o => { return o instanceof Food && o.IsEdibleBy(this) && !( this.ignore_list && this.ignore_list.has(o) ) };
-					foods = this.tank.grid.GetObjectsByBox( this.x - r, this.y - r, this.x + r, this.y + r, test );				
-				}
+				const test = o => { return o instanceof Food && o.IsEdibleBy(this) && !( this.ignore_list && this.ignore_list.has(o) ) };
+				foods = this.tank.grid.GetObjectsByBox( this.x - r, this.y - r, this.x + r, this.y + r, test );				
 				// check for collision + edibility
 				for ( let food of foods ) { 
 					const dx = Math.abs(food.x - this.x);
